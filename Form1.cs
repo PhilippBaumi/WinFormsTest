16d015aa09d5eaeb35a1d60f83caa6180dab2846
PhilippBaumi <philipp.baumgartner@live.at>
2026-03-28 00:21:42 +0100

    Codebereinigung ausgeführt

--- Form1.cs
+++ Form1.cs
@@ -4,7 +4,7 @@ namespace WinFormsTest
 {
     public partial class WinFormsTest : Form
     {
-        private ObservableCollection<string>dates=new();
+        private ObservableCollection<string> dates = new();
         public WinFormsTest()
         {
             InitializeComponent();
@@ -15,15 +15,15 @@ namespace WinFormsTest
             ListView.SelectedListViewItemCollection dates = datesList.SelectedItems;
             if (dates.Count == 1)
             {
-                ListViewItem item= dates[0];
-                MessageBox.Show("Du hast den "+item.Text+" gewählt! ","Datum gewählt", MessageBoxButtons.OK, MessageBoxIcon.Information);
+                ListViewItem item = dates[0];
+                MessageBox.Show("Du hast den " + item.Text + " gewählt! ", "Datum gewählt", MessageBoxButtons.OK, MessageBoxIcon.Information);
             }
         }
 
         private void btnAdd_Click(object sender, EventArgs e)
         {
-            string s=datePickerDate.Value.ToString("dd.MM.yyyy").Trim();
-            if(!dates.Contains(s)) 
+            string s = datePickerDate.Value.ToString("dd.MM.yyyy").Trim();
+            if (!dates.Contains(s))
             {
                 dates.Add(s);
                 datesList.Items.Clear();
@@ -34,7 +34,7 @@ namespace WinFormsTest
             }
             else
             {
-                MessageBox.Show("Datum " + s + " bereits vorhanden","Datum vorhanden", MessageBoxButtons.OK, MessageBoxIcon.Information);
+                MessageBox.Show("Datum " + s + " bereits vorhanden", "Datum vorhanden", MessageBoxButtons.OK, MessageBoxIcon.Information);
             }
         }
     }
